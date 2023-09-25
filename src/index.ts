24bb9a57505390d3ea714d72e172fef4fd2c22f4
Shimiz <shimi.zacken@gmail.com>
2023-09-25 10:13:10 +0200

    remove await

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import { runSSOFlow } from "./runSSOFlow";
 import { Auth0_SPA_JS_CDN, ZENDESK_WIDGET_CDN } from "./constants";
 import { loadScript } from "./loadScript";
 
-await loadScript(Auth0_SPA_JS_CDN, () => {
+loadScript(Auth0_SPA_JS_CDN, () => {
   console.log("🚀 ~ auth0 loaded");
+}).then(() => {
+  loadScript(
+    ZENDESK_WIDGET_CDN,
+    () => {
+      console.log("🚀 ~ window.zE", window.zE);
+      runSSOFlow();
+    },
+    "ze-snippet"
+  );
 });
-
-await loadScript(
-  ZENDESK_WIDGET_CDN,
-  () => {
-    console.log("🚀 ~ window.zE", window.zE);
-    runSSOFlow();
-  },
-  "ze-snippet"
-);
