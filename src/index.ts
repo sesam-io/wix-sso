4ba7fabf7663a10c8ae5dd9aff0232a3e4f53800
Shimiz <shimi.zacken@gmail.com>
2023-09-25 10:09:11 +0200

    fix: import

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import { runSSOFlow } from "runSSOFlow";
+import { runSSOFlow } from "./runSSOFlow";
 import { Auth0_SPA_JS_CDN, ZENDESK_WIDGET_CDN } from "./constants";
 import { loadScript } from "./loadScript";
 
-await loadScript(Auth0_SPA_JS_CDN, () => {});
+await loadScript(Auth0_SPA_JS_CDN, () => {
+  console.log("🚀 ~ auth0 loaded");
+});
+
 await loadScript(
   ZENDESK_WIDGET_CDN,
   () => {
