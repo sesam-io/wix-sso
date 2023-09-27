e36652085085e4d111eacfb9aa34a4ce713392fc
Branislav Jenco <branislav.jenco@sesam.io>
2023-09-27 10:08:20 +0200

    [IS-15952] Setting user-id in Google Analytics

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,9 @@ export type LoadScriptArgs = {
   idAttribute?: string;
   url: string;
 };
+
+// The datalayer object is set on the window object by the Google Analytics script
+// it can be used to configure GA, set options or send specific events 
+declare global {
+  interface Window { dataLayer: any[]; }
+}
