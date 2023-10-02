f8cf1db9f18114f65103ab93bf2879d4ead1ccf6
Shimiz <shimi.zacken@gmail.com>
2023-10-02 20:13:38 +0200

    enhancement

--- src/runSSOFlow.ts
+++ src/runSSOFlow.ts
@@ -33,29 +33,26 @@ export const runSSOFlow = (args: RunSSOFlowArgs) => {
       return;
     }
 
-    // const query = window.location.search;
+    const query = window.location.search;
 
-    // if (query.includes("code=") && query.includes("state=")) {
-    //   const redirectLoginResult = await auth0Client.handleRedirectCallback<{
-    //     target: string;
-    //   }>();
+    if (query.includes("code=") && query.includes("state=")) {
+      const redirectLoginResult = await auth0Client.handleRedirectCallback<{
+        target: string;
+      }>();
 
-    //   await updateHttpFunctions(auth0Client, auth0Id);
+      await updateHttpFunctions(auth0Client, auth0Id);
 
-    //   log(
-    //     "handleRedirectCallback ~ appState:",
-    //     redirectLoginResult.appState?.target
-    //   );
+      log(
+        "handleRedirectCallback ~ appState:",
+        redirectLoginResult.appState?.target
+      );
 
-    //   window.history.replaceState(
-    //     {},
-    //     "",
-    //     redirectLoginResult.appState?.target ?? window.location.origin
-    //   );
-
-    //   // window.location.href =
-    //   //   redirectLoginResult.appState?.target ?? window.location.origin;
-    // }
+      window.history.replaceState(
+        {},
+        "",
+        redirectLoginResult.appState?.target ?? window.location.origin
+      );
+    }
   };
 
   window.addEventListener("message", async (event) => {
@@ -85,26 +82,13 @@ export const runSSOFlow = (args: RunSSOFlowArgs) => {
 
         const query = window.location.search;
 
-        if (query.includes("code=") && query.includes("state=")) {
+        if (await auth0Client.isAuthenticated()) {
           const redirectLoginResult = await auth0Client.handleRedirectCallback<{
             target: string;
           }>();
 
-          await updateHttpFunctions(auth0Client, auth0Id);
-
-          log(
-            "handleRedirectCallback ~ appState:",
-            redirectLoginResult.appState?.target
-          );
-
-          window.history.replaceState(
-            {},
-            "",
-            redirectLoginResult.appState?.target ?? window.location.origin
-          );
-
-          // window.location.href =
-          //   redirectLoginResult.appState?.target ?? window.location.origin;
+          window.location.href =
+            redirectLoginResult.appState?.target ?? window.location.origin;
         }
       } else {
         updateHttpFunctions(auth0Client, auth0Id);
