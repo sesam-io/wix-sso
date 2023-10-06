e1f7143adf2808caf742b6aae0d07ed9ca82cdbd
Shimiz <shimi.zacken@gmail.com>
2023-10-06 09:56:38 +0200

    test: cases added

--- src/utils/calculateSSOFlowStatus.test.ts
+++ src/utils/calculateSSOFlowStatus.test.ts
@@ -1,6 +1,15 @@
 import { calculateSSOFlowStatus } from "./calculateSSOFlowStatus";
 
 describe("calculateSSOFlowStatus test suite", () => {
+  it("should return LoggedOut", () => {
+    const args = {
+      isAuthenticated: false,
+      query: "https://www.wave.sesam.io",
+    };
+
+    expect(calculateSSOFlowStatus(args)).toMatchSnapshot();
+  });
+
   it("should return Authenticated", () => {
     const args = {
       isAuthenticated: false,
