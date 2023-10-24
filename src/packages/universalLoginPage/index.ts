bd8d032328fc7fcd651399287b74a8c2c2c0eceb
Shimiz <shimi.zacken@gmail.com>
2023-10-24 19:28:57 +0200

    refactor and build release

--- src/packages/universalLoginPage/index.ts
+++ src/packages/universalLoginPage/index.ts
@@ -10,14 +10,14 @@ import { LOGO_IMG_ID, SiteIds } from "./constants";
 // const siteId = "{{transaction.params.ext-site_id}}" || SiteIds.sesam;
 
 // @ts-ignore
-if (window.formType && window.siteId) {
+if (formType && siteId) {
   // @ts-ignore
-  const site = getWixSite(window.siteId);
+  const site = getWixSite(siteId);
 
   const brandTitle = getBrandTitleFn(
     document.getElementsByTagName("p") ?? {},
     // @ts-ignore
-    getDefaultPageTitle(window.formType, window.siteId)
+    getDefaultPageTitle(formType, siteId)
   );
 
   const imgElement = document.getElementById(LOGO_IMG_ID) as HTMLImageElement;
