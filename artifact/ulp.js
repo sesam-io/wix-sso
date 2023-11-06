76fef619ed042f6e4624ea7e8b90d68cf1a813ed
Shimiz <shimi.zacken@gmail.com>
2023-11-06 15:02:20 +0100

    build

--- artifact/ulp.js
+++ artifact/ulp.js
@@ -263,7 +263,7 @@ const SiteIds = {
     tripletex: "Tripletex",
     wave: "Wave"
 };
-const STABLE_VERSION = "v1.0.116-site-logos";
+const STABLE_VERSION = "v1.0.117-site-logos";
 const SITE_LOGOS_RELEASE_VERSION = localStorage.getItem(SITE_LOGOS_VERSION_KEY) ?? STABLE_VERSION;
 const BASE_LOGO_URL = `https://cdn.jsdelivr.net/gh/sesam-io/wix-sso@${SITE_LOGOS_RELEASE_VERSION}/src/packages/siteLogos`;
 const DEFAULT_LOGO_URL = `${BASE_LOGO_URL}/sesam-talk-rgb.png`;
