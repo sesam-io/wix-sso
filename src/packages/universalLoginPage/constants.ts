2d4c0c0fdf1f53671346b92ec571faccd132685b
Shimiz <shimi.zacken@gmail.com>
2024-02-27 10:38:04 +0100

    chore: tt test site id added

--- src/packages/universalLoginPage/constants.ts
+++ src/packages/universalLoginPage/constants.ts
@@ -1,20 +1,22 @@
-import type { Sites } from './types';
+import type { Sites } from "./types";
 
-const SITE_LOGOS_VERSION_KEY = '_v_';
-export const LOGO_IMG_ID = 'prompt-logo-center';
+const SITE_LOGOS_VERSION_KEY = "_v_";
+export const LOGO_IMG_ID = "prompt-logo-center";
 
 export const SiteIds = {
-  hubspot: 'HubSpot',
-  powerofficego: 'PowerOfficeGo',
-  sesam: 'Sesam',
-  superoffice: 'SuperOffice',
-  tripletex: 'Tripletex',
-  wave: 'Wave',
+  hubspot: "HubSpot",
+  powerofficego: "PowerOfficeGo",
+  sesam: "Sesam",
+  superoffice: "SuperOffice",
+  tripletex: "Tripletex",
+  "tripletex-test": "Tripletex test",
+  wave: "Wave",
 };
 
-const STABLE_VERSION = 'v1.1.2';
+const STABLE_VERSION = "v1.1.2";
 
-const SITE_LOGOS_RELEASE_VERSION = localStorage.getItem(SITE_LOGOS_VERSION_KEY) ?? STABLE_VERSION;
+const SITE_LOGOS_RELEASE_VERSION =
+  localStorage.getItem(SITE_LOGOS_VERSION_KEY) ?? STABLE_VERSION;
 
 export const BASE_LOGO_URL = `https://cdn.jsdelivr.net/gh/sesam-io/wix-sso@${SITE_LOGOS_RELEASE_VERSION}/src/packages/siteLogos`;
 
@@ -22,40 +24,46 @@ const DEFAULT_LOGO_URL = `${BASE_LOGO_URL}/sesam/sesam-talk-rgb.png`;
 
 export const WixSites: Sites = {
   hubspot: {
-    id: 'hubspot',
+    id: "hubspot",
     logoUrl: `${BASE_LOGO_URL}/hubspot/making-hubspot-talk-logo-centered.svg`,
-    loginSubTitle: 'Log in to Making HubSpot Talk.',
-    signupSubTitle: 'Sign up to Making HubSpot Talk.',
+    loginSubTitle: "Log in to Making HubSpot Talk.",
+    signupSubTitle: "Sign up to Making HubSpot Talk.",
   },
   powerofficego: {
-    id: 'powerofficego',
+    id: "powerofficego",
     logoUrl: `${BASE_LOGO_URL}/poweroffice/poweroffice-ulp-header-logo.svg`,
-    loginSubTitle: 'Log in to PowerOffice DataSync.',
-    signupSubTitle: 'Sign up to PowerOffice DataSync.',
+    loginSubTitle: "Log in to PowerOffice DataSync.",
+    signupSubTitle: "Sign up to PowerOffice DataSync.",
   },
   sesam: {
-    id: 'sesam',
+    id: "sesam",
     logoUrl: DEFAULT_LOGO_URL,
-    loginSubTitle: 'Log in to Sesam Talk.',
-    signupSubTitle: 'Sign up to Sesam Talk to continue to Sesam Talk.',
+    loginSubTitle: "Log in to Sesam Talk.",
+    signupSubTitle: "Sign up to Sesam Talk to continue to Sesam Talk.",
   },
   superoffice: {
-    id: 'superoffice',
+    id: "superoffice",
     logoUrl: `${BASE_LOGO_URL}/superoffice/superoffice-ulp-header-logo.svg`,
-    loginSubTitle: 'Log in to SuperOffice DataSync.',
-    signupSubTitle: 'Sign up to SuperOffice DataSync.',
-    titleClassName: 'superofficeLogInTitle',
+    loginSubTitle: "Log in to SuperOffice DataSync.",
+    signupSubTitle: "Sign up to SuperOffice DataSync.",
+    titleClassName: "superofficeLogInTitle",
   },
   tripletex: {
-    id: 'tripletex',
+    id: "tripletex",
     logoUrl: `${BASE_LOGO_URL}/tripletex/tripletex-ulp-header-logo.svg`,
-    loginSubTitle: 'Log in to Tripletex DataSync.',
-    signupSubTitle: 'Sign up to Tripletex DataSync.',
+    loginSubTitle: "Log in to Tripletex DataSync.",
+    signupSubTitle: "Sign up to Tripletex DataSync.",
+  },
+  "tripletex-test": {
+    id: "tripletex-test",
+    logoUrl: `${BASE_LOGO_URL}/tripletex/tripletex-ulp-header-logo.svg`,
+    loginSubTitle: "Log in to Tripletex DataSync.",
+    signupSubTitle: "Sign up to Tripletex DataSync.",
   },
   wave: {
-    id: 'wave',
+    id: "wave",
     logoUrl: `${BASE_LOGO_URL}/wave/making-wave-talk-logo-centered.svg`,
-    loginSubTitle: 'Log in to Making Wave Talk.',
-    signupSubTitle: 'Sign up to Making Wave Talk.',
+    loginSubTitle: "Log in to Making Wave Talk.",
+    signupSubTitle: "Sign up to Making Wave Talk.",
   },
 };
