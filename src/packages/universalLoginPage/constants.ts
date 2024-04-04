e9acd1844b86419a034b059e66aebb3f3011f1a9
Shimiz <shimi.zacken@gmail.com>
2024-04-04 11:26:56 +0200

    new tt logo

--- src/packages/universalLoginPage/constants.ts
+++ src/packages/universalLoginPage/constants.ts
@@ -8,13 +8,15 @@ const STABLE_VERSION = "v1.0.127-site-logos";
 const SITE_LOGOS_RELEASE_VERSION =
   localStorage.getItem(SITE_LOGOS_VERSION_KEY) ?? STABLE_VERSION;
 
+export const CONNECTOR_LOGO_BASE_URL =
+  "https://raw.githubusercontent.com/sesam-io";
 export const BASE_LOGO_URL = `https://cdn.jsdelivr.net/gh/sesam-io/wix-sso@${SITE_LOGOS_RELEASE_VERSION}/src/packages/siteLogos`;
 
 const DEFAULT_LOGO_URL = `${BASE_LOGO_URL}/sesam/sesam-talk-rgb.png`;
 
 const tripletexSite: Site = {
   id: "tripletex",
-  logoUrl: `${BASE_LOGO_URL}/tripletex/tripletex-ulp-header-logo.svg`,
+  logoUrl: `${CONNECTOR_LOGO_BASE_URL}/tripletex-connector/main/assets/tripletex-logo-horizontal-complete.svg`,
   loginSubTitle: "Log in to Tripletex DataSync.",
   signupSubTitle: "Sign up to Tripletex DataSync.",
 };
